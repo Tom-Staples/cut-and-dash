83faae10deb49acb605bca9f177de26d545f4693
Tom Staples <staples.2007@yahoo.co.uk>
2023-05-16 13:44:57 +0100

    SideNav UI and router functionality implemented

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
-<script setup></script>
+<script setup>
+	import SideNav from './components/SideNav.vue';
+	import { RouterLink } from 'vue-router';
+</script>
 
 <template>
-	<h1>App is working</h1>
+	<div id="navContainer">
+		<RouterLink to="dashboard"><h1>Cut-and-Dash</h1></RouterLink>
+		<SideNav />
+	</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+	#navContainer {
+		display: flex;
+		flex-direction: column;
+		width: 300px;
+	}
+	#navContainer h1 {
+		margin: 20px 20px;
+	}
+	#navContainer a {
+		text-decoration: none;
+		color: rgb(97, 199, 237);
+	}
+</style>
---
+++ src/components/SideNav.vue
@@ -0,0 +1,52 @@
+<script setup>
+	import { RouterLink } from 'vue-router';
+	const navIcons = {
+		dashboard: 'space_dashboard',
+		schedule: 'calendar_month',
+		customers: 'groups',
+		barbers: 'account_box',
+		reports: 'lab_profile',
+		users: 'group_add'
+	};
+</script>
+
+<template>
+	<nav id="sideNav">
+		<ul v-for="link in Object.keys(navIcons)" :key="link">
+			<RouterLink :to="link"
+				><li>
+					<span class="material-symbols-outlined"> {{ navIcons[link] }} </span>
+					<span>{{ link[0].toUpperCase() + link.substring(1) }}</span>
+				</li></RouterLink
+			>
+		</ul>
+	</nav>
+</template>
+
+<style>
+	#sideNav ul {
+		list-style-type: none;
+		position: relative;
+	}
+	#sideNav li {
+		border-top-right-radius: 10px;
+		border-bottom-right-radius: 10px;
+		padding: 20px;
+		margin: 10px -40px;
+		width: 225px;
+	}
+	#sideNav a {
+		color: #000000;
+		text-decoration: none;
+	}
+	#sideNav .router-link-active.router-link-exact-active li {
+		background: rgb(97, 199, 237);
+		color: #ffffff;
+	}
+	.material-symbols-outlined {
+		font-variation-settings: 'FILL' 0, 'wght' 600, 'GRAD' 0, 'opsz' 48;
+		position: relative;
+		top: 5px;
+		right: 5px;
+	}
+</style>
