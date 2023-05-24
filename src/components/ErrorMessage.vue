5a07d2d6b1d2d88d0ecf0c1d47e881393a6fbbba
Tom Staples <staples.2007@yahoo.co.uk>
2023-05-24 12:43:01 +0100

    ErrorMessage component

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,15 @@
+<script setup>
+	defineProps(['message']);
+</script>
+
+<template>
+	<span class="errorMessage">Error: {{ message }}</span>
+</template>
+
+<style>
+	.errorMessage {
+		text-align: center;
+		margin: 10px 0;
+		color: red;
+	}
+</style>
