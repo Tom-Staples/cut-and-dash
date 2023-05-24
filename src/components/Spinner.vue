87d46ba6e608ec13269e190ee8699f28b6c41000
Tom Staples <staples.2007@yahoo.co.uk>
2023-05-24 12:18:37 +0100

    Spinner component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,50 @@
+<script setup></script>
+
+<template>
+	<span class="loader"></span>
+</template>
+
+<style>
+	.loader {
+		width: 25px;
+		height: 25px;
+		border-radius: 50%;
+		position: relative;
+		bottom: 60px;
+		left: 294px;
+		animation: rotate 1s linear infinite;
+	}
+	.loader::before {
+		content: '';
+		box-sizing: border-box;
+		position: absolute;
+		inset: 0px;
+		border-radius: 50%;
+		border: 5px solid rgb(97, 199, 237);
+		animation: prixClipFix 2s linear infinite;
+	}
+
+	@keyframes rotate {
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+
+	@keyframes prixClipFix {
+		0% {
+			clip-path: polygon(50% 50%, 0 0, 0 0, 0 0, 0 0, 0 0);
+		}
+		25% {
+			clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 0, 100% 0, 100% 0);
+		}
+		50% {
+			clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 100% 100%, 100% 100%);
+		}
+		75% {
+			clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 0 100%, 0 100%);
+		}
+		100% {
+			clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 0 100%, 0 0);
+		}
+	}
+</style>
