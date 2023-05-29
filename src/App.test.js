474b6cd623a02ef26f2c64bd3be46a95517016d6
Tom Staples <staples.2007@yahoo.co.uk>
2023-05-29 09:27:18 +0100

    added extra test for app

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,22 @@
 import { shallowMount } from '@vue/test-utils';
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, test } from 'vitest';
 import App from './App.vue';
 
 describe('App tests', () => {
-	const wrapper = shallowMount(App);
-	it('does not render div by default', () => {
+	beforeEach(() => {
+		localStorage.clear();
+	});
+
+	test('does not render div by default', () => {
+		const wrapper = shallowMount(App);
 		expect(wrapper.find('#navContainer').exists()).toBeFalsy();
+	});
+	test('does render div when token in local storage', () => {
+		localStorage.setItem(
+			'token',
+			JSON.stringify({ access_token: 'tjjadjhdbjha' })
+		);
+		const wrapper = shallowMount(App);
+		expect(wrapper.find('#navContainer').exists()).toBeTruthy();
 	});
 });
